731c65f951cf3ba1797dc4afd71ed2224362c43d
Prince <princemultaniya89@gmail.com>
2024-11-29 20:11:12 +0530

    foo

--- TEN-virtual-incubator-main/backend/Middleware/catchasyncerrors.js
+++
@@ -1,4 +0,0 @@
-//For Async Function Errors in PropertyControllers
-module.exports = func =>(req,res,next)=>{
-    Promise.resolve(func(req,res,next)).catch(next);
-}
--- TEN-virtual-incubator-main/backend/routes/contact.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const contactRouter = express.Router()
-import { contactUsController }  from "../controllers/ContactUsContoller.js"
-
-contactRouter.post("/contact", contactUsController)
-
-export default contactRouter;
